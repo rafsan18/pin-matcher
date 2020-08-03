4252852dd6dd7ac6a83ee645a92817dc51cd5343
rafsan <rafsanamin18@gmail.com>
2020-08-03 07:15:18 +0600

    successful trial of getting number

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
 const pinGeneratorBtn = document.getElementById("pin-generator-btn");
-const pinDisplay = document.getElementById("pin-display");
-pinGeneratorBtn.addEventListener("click", function () {
-  let pinNumber = Math.floor(Math.random() * (9999 - 1000) + 1000);
-  pinDisplay.value = pinNumber;
-});
+const generatedPinDisplay = document.getElementById("generated-pin-display");
+const number = document.querySelectorAll("[data-number]");
 
+// Random pin Generator (start)
+pinGeneratorBtn.addEventListener("click", function () {
+  let pinNumber = Math.floor(Math.random() * (9999 - 1000) + 1000); //generated pin between 1000 and 9999.
+  generatedPinDisplay.value = pinNumber;
+});
+// Random pin Generator (end)
+
+for (let i = 0; i < number.length; i++) {
+  const numberBtn = number[i];
+  numberBtn.addEventListener("click", function () {
+    console.log(numberBtn.innerText);
+  });
+}
 // practice
 
 // const submitBtn = document.querySelector("[data-submit]");
